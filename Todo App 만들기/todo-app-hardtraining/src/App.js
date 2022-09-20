9bfce6c0a9dd09e0096015225f3ccbd9c4185ae5
biyamn <b1y9mn@gmail.com>
2022-09-21 00:01:46 +0900

    hardtraining

--- Todo App 만들기/todo-app-hardtraining/src/App.js
+++ Todo App 만들기/todo-app-hardtraining/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 
 function App() {
+  const handleSubmit = () => {
+
+  }
+
   return (
     <div className='container'>
       <div className='contents'>
         <h1>할 일 목록</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div className='lists'>
+            <input className='checkbox' type='checkbox' />
+            <span className='listContent'>청소하기</span>
+            <button className='deleteBtn'>x</button>
+          </div>
+
+          <div className='inputs'>
+            <input className='inputText' type='text' placeholder='해야 할 일을 입력하세요.' />
+            <input className='inputSubmit' type='submit' value='입력'/>
+          </div>
+        </form>
+
       </div>
     </div>
   );
