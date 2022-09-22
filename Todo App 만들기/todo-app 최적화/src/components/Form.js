ebd81a992e10287cd94641f1e6b6ed20c27227c1
biyamn <b1y9mn@gmail.com>
2022-09-23 05:19:56 +0900

    drag and drop

--- Todo App 만들기/todo-app 최적화/src/components/Form.js
+++ Todo App 만들기/todo-app 최적화/src/components/Form.js
@@ -7,11 +7,12 @@ const Form = ({ handleSubmit, value, setValue }) => {
   };
 
   return (
-    <form style={{display:'flex'}} onSubmit={handleSubmit}>
+    
+    <form onSubmit={handleSubmit} className="flex pt-2">
       <input 
         type="text" 
-        name="value" 
-        style={{flex: '10', padding: '5px'}}
+        name="value"
+        className="w-full px-3 py-2 mr-4 text-gray-500 border rounded"
         placeholder="해야 할 일을 입력하세요."
         value={value}
         onChange={handleChange}
@@ -19,8 +20,7 @@ const Form = ({ handleSubmit, value, setValue }) => {
         <input 
           type="submit"
           value="입력"
-          className="btn"
-          style={{flex: '1'}}
+          className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-200"
         />
     </form>
   );
--- Todo App 만들기/todo-app 최적화/src/components/List.js
+++ Todo App 만들기/todo-app 최적화/src/components/List.js
@@ -1,15 +1,7 @@
 import React from 'react';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
 
 function List ({ todoData, setTodoData }) {
-
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right"
-  };
 
   const handleCompleteChange = (id) => {
     let newTodoData = todoData.map(data => {
@@ -17,16 +9,8 @@ function List ({ todoData, setTodoData }) {
         data.completed = !data.completed;
       }
       return data;
-    })
+    });
     setTodoData(newTodoData);
-  };
-
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    }
   };
 
   const handleClick = (id) => {
@@ -35,15 +19,69 @@ function List ({ todoData, setTodoData }) {
     setTodoData(newTodoData);
   };
 
+  const handleEnd = (result) => {
+    // result 매개변수에는 source 항목 및 대상 위치와 같은 드래그 이벤트에 대한 정보가 포함된다.
+    // console.log(result);
+
+    // 목적지가 없으면(이벤트 취소) 이 함수를 종료한다.
+    if (!result.destination) return;
+
+    const newTodoData = todoData;
+    
+    // 1. 변경시키는 아이템을 배열에서 지워준다.
+    // 2. return 값으로 지워진 아이템을 잡아준다.
+    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+
+    // 원하는 자리에 reorderdItem을 insert해준다.
+    newTodoData.splice(result.destination.index, 0, reorderedItem);
+    setTodoData(newTodoData);
+
+  };
+
   return (
     <div>
-      {todoData.map((data) => (
-        <div style={getStyle(data.completed)} key={data.id}>
-          <input type="checkbox" defaultChecked={false} onChange={() => handleCompleteChange(data.id)}/>
-            {data.title}
-          <button style={btnStyle} onClick={() => handleClick(data.id)}>x</button>
-        </div>
-      ))}
+      <DragDropContext onDragEnd={handleEnd}>
+        <Droppable droppableId="todo">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => (
+                <Draggable
+                  key={data.id}
+                  draggableId={data.id.toString()}
+                  index={index}
+              >
+                  
+                  {(provided, snapshot) => (
+                    <div 
+                      key={data.id} 
+                      {...provided.draggableProps}
+                      ref={provided.innerRef} 
+
+                      {...provided.dragHandleProps}
+                      className={`${
+                        snapshot.isDragging ? "bg-gray-300" : "bg-gray-100"
+                      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
+                    >
+                        <div className='items-center'>
+                          <input type="checkbox" 
+                            defaultChecked={false} 
+                            onChange={() => handleCompleteChange(data.id)}
+                          />{" "}
+                          <span className={data.completed ? "line-through": undefined}>{data.title}</span>
+                        </div>
+
+                        <div className='items-center'>
+                        <button className="px-4 py-2 float-right" onClick={() => handleClick(data.id)}>x</button>
+                        </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
