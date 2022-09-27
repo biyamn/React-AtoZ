b48736a7c161b152c2ffd534e6624f0b47b6910b
biyamn <b1y9mn@gmail.com>
2022-09-27 20:50:16 +0900

    할 일 추가하기 기능 개발 중

--- Todo App 만들기/todo-app-hardtraining/src/App.js
+++ Todo App 만들기/todo-app-hardtraining/src/App.js
@@ -2,14 +2,24 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  const [state, setState] = useState('');
+  const [text, setText] = useState('');
 
-  const onChange = (e) => {
-    setState(e.target.value);
+  const [todoList, setTodoList] = useState([]);
+
+  const onChangeInput = (e) => {
+    setText(e.target.value);
   };
 
   const onSubmit = (e) => {
-    setState('');
+    const nextTodoList = todoList.concat({
+      id: todoList.length,
+      text,
+      checked: false,
+    });
+
+    setTodoList(nextTodoList);
+
+    setText('');
     e.preventDefault();
   };
 
@@ -18,18 +28,33 @@ function App() {
       <div className='contents'>
         <h1>할 일 목록</h1>
 
+
         <form onSubmit={onSubmit}>
           <div className='lists'>
-            <input className='checkbox' type='checkbox' />
-            <span className='listContent'>청소하기</span>
-            <button className='deleteBtn'>x</button>
+            todoList.map((todoItem) =>(
+              <input className='checkbox' type='checkbox'/>
+              <span className='listContent'>{todoItem.text}</span>
+              <button className='deleteBtn'>x</button>
+            ))
           </div>
 
           <div className='inputs'>
-            <input onChange={onChange} className='inputText' type='text' placeholder='해야 할 일을 입력하세요.' />
-            <input className='inputSubmit' type='submit' value='입력'/>
+            <input 
+              onChange={onChangeInput} 
+              className='inputText' 
+              type='text' 
+              placeholder='해야 할 일을 입력하세요.'
+              value={text}
+              name='todoItem'
+            />
+            <input 
+              className='inputSubmit' 
+              type='submit' 
+              value='입력'
+            />
           </div>
         </form>
+
 
       </div>
     </div>
